65647b8e5e9624db65d411ced5bb025c6de7dbfb
aitorcuesta <aitor.cuesta@gmail.com>
2021-01-16 12:28:10 +0100

    Football World Cup Scoreboard initial test

---
+++ src/test/java/com/sportradar/acuesta/scoreboard/FootballWorldCupScoreboardTest.java
@@ -0,0 +1,34 @@
+package com.sportradar.acuesta.scoreboard;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Test class for Football World Cup Scoreboard
+ * @author acuesta
+ *
+ */
+public class FootballWorldCupScoreboardTest {
+    
+    @Test
+    public void gameCanBeStartedWithBothTeamNames() {
+	fail("Unimplemented!");
+    }
+    
+    @Test
+    public void finishedGamesAreRemovedFromScoreboard() {
+	fail("Unimplemented!");
+    }
+    
+    @Test
+    public void gamesCanBeUpdated() {
+	fail("Unimplemented!");
+    }
+    
+    @Test
+    public void gamesSummaryIsSortedByTotalScoreAndMostRecentlyAdded() {
+	fail("Unimplemented!");
+    }
+
+}
