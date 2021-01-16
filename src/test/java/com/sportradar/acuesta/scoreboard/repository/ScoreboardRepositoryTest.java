d10997eea9f43f1f5f4c09787b9317e3d151a7de
aitorcuesta <aitor.cuesta@gmail.com>
2021-01-16 10:48:46 +0100

    Intial test for in-memory repository

---
+++ src/test/java/com/sportradar/acuesta/scoreboard/repository/ScoreboardRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.sportradar.acuesta.scoreboard.repository;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ScoreboardRepositoryTest {
+
+    @Test
+    public void gamesCanBeAdded() {
+	fail("Uninplemented!");
+    }
+
+    @Test
+    public void nullGamesCannotBeAdded() {
+	fail("Uninplemented!");
+    }
+
+    public void addedGamesCanBeRetrieved() {
+	fail("Uninplemented!");
+    }
+
+    @Test
+    public void storedGamesCanBeDeleted() {
+	fail("Uninplemented!");
+    }
+
+    @Test
+    public void onlystoredGamesCanBeDeleted() {
+	fail("Uninplemented!");
+    }
+
+    @Test
+    public void storedGamesCanBeUpdated() {
+	fail("Uninplemented!");
+    }
+
+    @Test
+    public void onlyStoredGamesCanBeUpdated() {
+	fail("Uninplemented!");
+    }
+
+    @Test
+    public void allGamesCanBeRetrieved() {
+	fail("Uninplemented!");
+    }
+
+}
