9500cda4416bcf7025c40c209d9cd6d060f174b9
aitorcuesta <aitor.cuesta@gmail.com>
2021-01-16 09:53:48 +0100

    Initial test for games

---
+++ src/test/java/com/sportradar/acuesta/scoreboard/entity/GameTest.java
@@ -0,0 +1,39 @@
+package com.sportradar.acuesta.scoreboard.entity;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Test class for games
+ * @author acuesta
+ *
+ */
+public class GameTest {
+
+    @Test
+    public void gamesWithoutHomeTeamGenerateError() {
+	fail("Unimplemented!");
+    }
+    
+    @Test
+    public void gamesWithoutAwayTeamGenerateError() {
+	fail("Unimplemented!");
+    }
+
+    @Test
+    public void gameInitialScoreIs0_0() {
+	fail("Unimplemented!");
+    }
+
+    @Test
+    public void gameTotalScoreIsHomeScorePlusAwayScore() {
+	fail("Unimplemented!");
+    }
+    
+    @Test
+    public void gamesHaveCreationTime() {
+	fail("Unimplemented!");
+    }
+
+}
